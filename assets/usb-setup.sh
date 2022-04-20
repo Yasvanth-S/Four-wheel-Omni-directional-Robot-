7daea60c90b0b8cedb05d831672fd97a1e61c31c
gokul <gokulakams@gmail.com>
2022-04-20 09:39:25 +0530

    added two new motors

--- assets/usb-setup.sh
+++ assets/usb-setup.sh
@@ -2,3 +2,5 @@ echo 0001 0001 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
 echo 0002 0002 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
 echo 0003 0003 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
 echo 0004 0004 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 0005 0005 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+echo 0006 0006 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
