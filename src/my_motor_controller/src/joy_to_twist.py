a80620017d20f8e39657f6f87c3458a9e78a6218
yasvanth-s <yasvanth.senthilkumar@gmail.com>
2022-04-22 11:46:13 +0200

    Modified usb setup and joy to twist

--- src/my_motor_controller/src/joy_to_twist.py
+++ src/my_motor_controller/src/joy_to_twist.py
@@ -14,29 +14,33 @@ class JoyClass:
         self.wheel4_pub = rospy.Publisher("/wheel4_vel", Float32, queue_size=10)
         self.yaxis_pub = rospy.Publisher("/yaxis_vel", Float32, queue_size=10)
         self.zaxis_pub = rospy.Publisher("/zaxis_vel", Float32, queue_size=10)
-        self.test = rospy.Publisher("/test", Float32, queue_size=10)
+        #self.test = rospy.Publisher("/test", Float32, queue_size=10)
+        self.rate = rospy.Rate(10)
 
     def joy_callback(self, msg):
-        angular = 0.0
-        rightTrig = (abs(msg.axes[5]-1.0)/2) * 0.1
-        leftTrig = (-abs(msg.axes[2]-1.0)/2) * 0.1
-        #self.test.publish(leftTrig)
+        wheel1, wheel2, wheel3, wheel4, joyY, joyZ, angular = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        rightTrig = (abs(msg.axes[4]-1.0)/2) 
+        leftTrig = (-abs(msg.axes[3]-1.0)/2)
+        joyY = round(msg.axes[2],1)
+        joyZ = round(msg.axes[5],1)
         if(rightTrig!=0 and leftTrig!=0):
             angular = 0.0
         elif(rightTrig>0.0 and leftTrig == 0.0):
             angular = rightTrig
         elif(leftTrig<0.0 and rightTrig == 0.0):
             angular = leftTrig
-        wheel1 = (msg.axes[1]+0.5*angular)*0.1
-        wheel3 = (msg.axes[1]-0.5*angular)*0.1
-        wheel2 = (msg.axes[0]+0.5*angular)*0.1
-        wheel4 = (msg.axes[0]-0.5*angular)*0.1       
+        wheel1 = (msg.axes[0]+angular)*1.5
+        wheel3 = (-msg.axes[0]+angular)*1.5
+        wheel2 = (-msg.axes[1]+angular)*1.5
+        wheel4 = (msg.axes[1]+angular)*1.5      
         self.wheel1_pub.publish(wheel1)
         self.wheel2_pub.publish(wheel2)
         self.wheel3_pub.publish(wheel3)
         self.wheel4_pub.publish(wheel4)
-        self.yaxis_pub.publish(msg.axes[3]*0.1)
-        self.zaxis_pub.publish(msg.axes[4]*0.1)
+        if(joyY > 0.3 or joyY < -0.3 or joyY==0.0):
+            self.yaxis_pub.publish(msg.axes[2]*1.5)
+        if(joyZ > 0.3 or joyZ < -0.3 or joyZ==0.0):
+            self.zaxis_pub.publish(-(msg.axes[5]*1.2))
 
 if __name__=="__main__":
     try:
