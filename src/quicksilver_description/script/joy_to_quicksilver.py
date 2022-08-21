8462ee75439f3edf37191f1d2d93c04ec167c681
Yasvanth-S <yasvanth.senthilkumar@gmail.com>
2022-08-21 15:44:47 +0530

    published tf odom and cmd_vel is done and took gmapping navigation stack to be done

--- src/quicksilver_description/script/joy_to_quicksilver.py
+++ src/quicksilver_description/script/joy_to_quicksilver.py
@@ -3,6 +3,7 @@
 import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
 def constrain(val, min_val, max_val):
     return min(max_val, max(min_val, val))
 class JoyClass:
@@ -13,8 +14,24 @@ class JoyClass:
         self.wheel2_pub = rospy.Publisher("/quicksilver/rim_wheel3_joint/command", Float64, queue_size=10)
         self.wheel3_pub = rospy.Publisher("/quicksilver/rim_wheel4_joint/command", Float64, queue_size=10)
         self.wheel4_pub = rospy.Publisher("/quicksilver/rim_wheel1_joint/command", Float64, queue_size=10)
-
+        self.velocity_subscriber = rospy.Subscriber('/cmd_vel', Twist, self.velocity_callback)
         self.rate = rospy.Rate(10)
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def velocity_callback(self,msg):
+        self.x = msg.linear.x
+        self.y = msg.linear.y
+        self.z = msg.angular.z
+        wheel1 = (self.x/0.038) + (self.z /0.038)
+        wheel2 = (-self.y/0.038) + (self.z / 0.038)
+        wheel3 = (-self.x/0.038) + (self.z / 0.038) # 0.038 is wheel radius
+        wheel4 = (self.y/0.038) + (self.z / 0.038)#angular negative due to inward placement of wheels
+        self.wheel1_pub.publish(constrain(wheel1,-5.5,5.5))
+        self.wheel2_pub.publish(constrain(wheel2,-5.5,5.5))
+        self.wheel3_pub.publish(constrain(wheel3,-5.5,5.5))
+        self.wheel4_pub.publish(constrain(wheel4,-5.5,5.5))
 
     def joy_callback(self, msg):
         wheel1, wheel2, wheel3, wheel4, joyY, joyZ, angular = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
@@ -26,10 +43,10 @@ class JoyClass:
             angular = rightTrig
         elif(leftTrig<0.0 and rightTrig == 0.0):
             angular = leftTrig     
-        wheel1 = (msg.axes[1]*5.5) + (msg.axes[7]*5.5) + (angular * 3.6)
-        wheel2 = (-msg.axes[0]*5.5) + (-msg.axes[6]*5.5) + (angular * 3.6)
-        wheel3 = (-msg.axes[1]*5.5) + (-msg.axes[7]*5.5) + (angular * 3.6)
-        wheel4 = (msg.axes[0]*5.5) + (msg.axes[6]*5.5) + (angular * 3.6)  #angular negative due to inward placement of wheels
+        wheel1 = (msg.axes[1]*5) + (msg.axes[7]*5) + (angular * 3.6) + (self.y*5) + (self.z * 3.6)
+        wheel2 = (-msg.axes[0]*5) + (-msg.axes[6]*5) + (angular * 3.6) + (-self.x*5) + (self.z * 3.6)
+        wheel3 = (-msg.axes[1]*5) + (-msg.axes[7]*5) + (angular * 3.6) + (-self.y*5) + (self.z * 3.6)
+        wheel4 = (msg.axes[0]*5) + (msg.axes[6]*5) + (angular * 3.6) + (self.x*5) + (self.z * 3.6)#angular negative due to inward placement of wheels
         self.wheel1_pub.publish(constrain(wheel1,-5.5,5.5))
         self.wheel2_pub.publish(constrain(wheel2,-5.5,5.5))
         self.wheel3_pub.publish(constrain(wheel3,-5.5,5.5))
--- src/quicksilver_description/script/odom_pub.py
+++ src/quicksilver_description/script/odom_pub.py
@@ -4,6 +4,7 @@ import queue
 import rospy
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
+import tf
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 
 rospy.init_node('odom_pub')
@@ -11,24 +12,32 @@ odom_pub = rospy.Publisher('/odom', Odometry,queue_size=100)
 
 rospy.wait_for_service('/gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-
+br = tf.TransformBroadcaster()
 odom = Odometry()
-header = Header()
-header.frame_id = 'odom'
-
+now = rospy.Time.now()
 model = GetModelStateRequest()
-model.model_name = 'open_base'
+model.model_name = 'quicksilver'
 
-r = rospy.Rate(10)
+r = rospy.Rate(100)
 
 while not rospy.is_shutdown():
 
     result = get_model_srv(model)
+    msgx = result.pose.position.x
+    msgy = result.pose.position.y
+    msgth = result.pose.orientation.z
+    br.sendTransform((msgx, msgy, 0),
+                     (result.pose.orientation.x,result.pose.orientation.y,result.pose.orientation.z,result.pose.orientation.w),
+                     rospy.Time.now(),
+                     'origin_link',
+                     "odom")
+
     odom.pose.pose = result.pose 
     odom.twist.twist = result.twist
 
-    header.stamp = rospy.Time.now()
-    odom.header = header
+    odom.header.stamp = now
+    odom.header.frame_id = 'odom'
+    odom.child_frame_id = 'origin_link'
     odom_pub.publish(odom)
 
     r.sleep()
